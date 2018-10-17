1fe8573bbaf5cc4ccad667255220fc76e6f1ad88
khristova21 <42977135+khristova21@users.noreply.github.com>
2018-10-17 13:37:17 -0400

    Add files via upload

--- MainManipulator.java
+++ MainManipulator.java
@@ -6,33 +6,45 @@ public class MainManipulator
 	{
 		Scanner in = new Scanner(System.in);
 		StringManipulator changer = new StringManipulator();
-		int play = 0;
-		if (play == 0)
+		boolean play = true;
+		while(play)
+		{
 			
 			System.out.print("If you'd like to run the no vowels method type 1...\nIf you want to run the reverse method type 2: ");
 			int run = in.nextInt();
 			in.nextLine();
+			while(run != 1 &&  run !=2)
+			{
+				System.out.print("Sorry, that is not a 1 or a 2. Please enter 1 for no vowels, or 2 for reverse: ");
+				run = in.nextInt();
+				in.nextLine();
+				
+			}
 			
-			if (run== 1) //novowels
+			if (run == 1) //novowels
 			{	
 				System.out.print("Enter a word you'd like to use: ");
-				String originalStr =in.next();
-				in.nextLine();
+				String originalStr =in.nextLine();
 				System.out.println(changer.noVowels(originalStr));
 			}
 			else //reverse
 			{
 				System.out.print("Enter a word or phrase you'd like to use: ");
-				String originalStr = in.next();
-				in.nextLine();
+				String originalStr = in.nextLine();
 				System.out.println(changer.reverse(originalStr));
 			}
 			//ask to play again
-			System.out.print("\nIf you'd like to play again type 0, otherwise thank you!");
-			if (in.nextInt() != 0)
+			System.out.print("\nDo you want to try again(y/n)?");
+			String answer = in.next();
+			
+			if (answer == "y")
 			{
-				System.out.println("Have a nice day!");
+				play = true;
 			}
+			else
+				play = false;
+	
+			//still loops  with the while
 	}
-
+	}
 }
